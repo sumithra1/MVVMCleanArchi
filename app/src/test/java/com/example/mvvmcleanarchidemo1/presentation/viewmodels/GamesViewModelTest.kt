e54247ec65c3ca685b4c9b670af37c1fb8c4c0f3
sumithra1 <sumi.rks6@gmail.com>
2022-12-07 15:11:00 +0530

    Unit Test inprogress

--- app/src/test/java/com/example/mvvmcleanarchidemo1/presentation/viewmodels/GamesViewModelTest.kt
+++ app/src/test/java/com/example/mvvmcleanarchidemo1/presentation/viewmodels/GamesViewModelTest.kt
@@ -1,7 +1,18 @@
 package com.example.mvvmcleanarchidemo1.presentation.viewmodels
 
+import com.example.mvvmcleanarchidemo1.data.api.GamesAPIService
+import org.junit.Before
 
 
-internal class GamesViewModelTest{
+class GamesViewModelTest{
 
+     private lateinit var gamesAPIService: GamesAPIService
+
+     @Before
+     fun setup(){
+
+     }
+    fun tearDown(){
+
+    }
 }
